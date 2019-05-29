932000ef787a85c1d905b78295cdbd14fe5b54d4
thedoctormarc <36292102+thedoctormarc@users.noreply.github.com>
2019-05-29 11:56:36 +0200

    trigger to top of stadium (teleport)

---
+++ TheNewDome/TheNewDome/Assets/Scripts/Teleport.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+using System; 
+
+public class Teleport : MonoBehaviour
+{
+    public Transform targetPos;
+    public GameObject player; 
+
+    void OnTriggerEnter(Collider col)
+    {
+        player.transform.position = targetPos.transform.position; 
+    }
+}
