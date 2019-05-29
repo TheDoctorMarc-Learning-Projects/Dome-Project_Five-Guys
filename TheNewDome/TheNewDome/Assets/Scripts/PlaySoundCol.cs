b13f7833bd5978cacceb737a7aca9afcb35f5865
thedoctormarc <36292102+thedoctormarc@users.noreply.github.com>
2019-05-29 12:51:30 +0200

    3 tunnels cap

---
+++ TheNewDome/TheNewDome/Assets/Scripts/PlaySoundCol.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundCol : MonoBehaviour
+{
+
+    public AudioSource theSound; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        theSound = GetComponent<AudioSource> (); 
+    }
+
+  
+    void OnTriggerEnter(Collider col)
+    {
+        theSound.Play (); 
+    } 
+}
