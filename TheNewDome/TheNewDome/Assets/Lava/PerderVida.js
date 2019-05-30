54f5b88c57a8f52d62dce20398410f632d1a0b01
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2019-05-30 14:21:01 +0200

    FORGE ADDED TO NEW PROJECT

---
+++ TheNewDome/TheNewDome/Assets/Lava/PerderVida.js
@@ -0,0 +1,18 @@
+﻿var health : float = 10;
+var playerController : GameObject;
+
+function OnTriggerStay(collision: Collider) {
+   
+    if (collision.gameObject.tag == "Arma"){
+        health -= 0.1f;
+        
+    }
+}
+
+    function Update()
+    {
+        if(health <= 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
